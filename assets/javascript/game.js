f0a1bfe5be4169009253effa41570ba5cddb2a8d
Andrew Harding <dreweharding@gmail.com>
2018-07-07 01:55:32 -0400

    updated layout, began game code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,3 +9,38 @@
 // cov-2
 // cov-3
 // cov-4
+// Victory Achieved
+// You Died
+
+// The random number shown at the start of the game should be between 19 - 120
+// Each crystal should have a random hidden value between 1 - 12
+
+$(document).ready(function () {
+
+    // Global Variables
+    var winCounter = 0;
+    var lossCounter = 0;
+    var targetNumber = 0;
+    var currentScore = 0;
+
+    // Game Object
+    var gameFunction = function () {
+        this.newGame = function () {
+            targetNumber = Math.floor(Math.random()*(120 - 19 + 1) + 19);
+        }
+        this.checkWin = function () {
+            if (currentScore == targetNumber) {
+                winCounter++;
+                $("#win-counter").text(winCounter);
+                this.newGame(); //Ask about this
+            }
+        }
+
+    }
+    var gameObject = gameFunction({});
+
+    gameObject.newGame();
+
+    console.log(targetNumber);
+
+});
